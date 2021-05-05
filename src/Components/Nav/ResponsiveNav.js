548fa0bcd11bc7d342b787911afa98ba0a47fc43
Emmanuel Perrin Joseph <ejoseph3@masonlive.gmu.edu>
2021-05-04 22:04:19 -0400

    responsive nav init

---
+++ src/Components/Nav/ResponsiveNav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ResponsiveNav() {
+    return (
+        <div className="responsive__navBar">
+            
+        </div>
+    )
+}
+
+export default ResponsiveNav
